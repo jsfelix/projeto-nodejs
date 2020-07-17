6fb85b86d241e64a3eb91692b4c84b83a6c2ffda
Jefferson dos Santos Felix <jsfelix@gmail.com>
2020-07-17 19:06:56 -0300

    Extra: verifica se provider existe

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,7 +1,8 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
+import { getRepository, getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import User from '../models/User';
 
 interface Request {
   provider_id: string;
@@ -10,25 +11,31 @@ interface Request {
 
 class CreateAppointmentService {
   public async execute({ provider_id, date }: Request): Promise<Appointment> {
+    // Checks if appointment already exists
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-
     const appointmentDate = startOfHour(date);
-
     const findAppointmentInSameDate = await appointmentsRepository.findByDate(
       appointmentDate,
     );
-
     if (findAppointmentInSameDate) {
       throw Error('This appointment is already booked');
     }
 
+    // Checks if provider exists
+    const userRepository = getRepository(User);
+    const provider = await userRepository.findOne({
+      where: { id: provider_id },
+    });
+    if (!provider) {
+      throw Error('This provider does not exist');
+    }
+
+    // Create a new appointment
     const appointment = appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
     await appointmentsRepository.save(appointment);
-
     return appointment;
   }
 }
